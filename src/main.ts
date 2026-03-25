8aa80526c9644d7d2e753833e9a3074f86cdda2d
Daxzyy <122425004+Daxzyy@users.noreply.github.com>
2026-03-25 17:27:22 +0700

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient()
-  ]
+  providers: []
 }).catch(err => console.error(err));
